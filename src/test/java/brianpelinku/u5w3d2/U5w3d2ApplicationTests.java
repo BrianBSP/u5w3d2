26a1291d82301b43edd6e04e3b83e8aa970d3b9d
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-09-17 14:40:14 +0200

    Initial commit

---
+++ src/test/java/brianpelinku/u5w3d2/U5w3d2ApplicationTests.java
@@ -0,0 +1,13 @@
+package brianpelinku.u5w3d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w3d2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
